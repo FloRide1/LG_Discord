3592bfc0fdeb80dc622fd271833184864f3c4671
sackboys 1 <florian.reimat@orange.fr>
2020-04-01 15:45:56 +0200

    0

--- lg.py
+++ lg.py
@@ -1,38 +1,4 @@
 import discord
 
-class Bot(discord.Client):
-    
-    def __init__ (self):
-        super().__init__()
+TOKEN = "Njk0ODg2NTE2MTE5NDM3NDEy.XoSMgw.9vsUoXKu_OWIzIszJ9yHlkUNH-8"
 
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
-
-    async def on_message(self, message):
-        
-        if(message.author == self.user):
-            return
-        if(message.content.startswith("lg!Jouer")):
-            await message.channel.send("Bonjour et bienvenue à Thiercelieux, combien de joueurs êtes vous?")
-
-## Test envoie dm
-
-        if(message.content.startswith("lg!dm")):
-
-            member = discord.utils.get(message.guild.members, name=message.content.split(" ")[1])
-
-            try:
-                await member.send("Bienvenue dans le test Loup Garou")
-            except discord.Forbidden:
-                await message.channel.send("Le joueur " + str(member) + " dois activer ses messages privés")
-            except AttributeError:
-                await message.channel.send("Le joueur " + str(member) + " n'est pas dans le village")
-            
-
-if __name__ == "__main__":
-
-    Loup_Garou_1 = Bot()
-    Loup_Garou_1.run("Njk0NTc1NTU3MDQzNTUyMzA3.XoSC_A.hJPwWxEhxyVoQZm0KWtk79nH7Ko")
