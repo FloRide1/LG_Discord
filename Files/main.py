46f5965039b22a0da2fa272a943650e02b787ee2
sackboys 1 <florian.reimat@orange.fr>
2020-04-01 18:06:10 +0200

    1

--- Files/main.py
+++ Files/main.py
@@ -1,6 +1,6 @@
 import discord
 
-TOKEN = "Njk0ODg2NTE2MTE5NDM3NDEy.XoSpUA.7MEMBPkTwIWraaBLQLyUIKWpUnw"
+TOKEN = "Njk0ODg2NTE2MTE5NDM3NDEy.XoS0hQ.2AR27Alth1pT39tyr9OB3snGDxI"
 
 class DiscordBot(discord.Client) :
     def __init__(self):
@@ -25,13 +25,13 @@ class DiscordBot(discord.Client) :
             if (not(self.partyIsLaunch)):
                 self.partyIsLaunch = True
                 self.partyPlayer = []
-                self.owner = message.author
+                self.owner = message.author.name
                 await message.channel.send("Qui veut jouer ?")
             else :
                 await message.channel.send("Une partie est déja lancée")
 
         if (message.content.startswith("lg!end")):
-            if (self.owner == message.author):
+            if (self.owner == message.author.name):
                 self.partyPlayer = []
                 self.partyIsLaunch = False
                 await message.channel.send("Fin de la partie")
@@ -44,11 +44,14 @@ class DiscordBot(discord.Client) :
         if (reaction.message.content == "Qui veut jouer ?" and reaction.emoji == "👍"):
             self.partyPlayer.append(user.name)
         if (reaction.message.content == "Qui veut jouer ?" and reaction.emoji == "▶️"):
-            if (reaction.message.author == self.owner):
-                await reaction.message.channel.send("Liste des joueurs")
-                for i in self.partyPlayer:
-                    await reaction.message.channel.send("- " + i)
+            if (user.name == self.owner):                
+                await reaction.message.channel.send("Liste des joueurs :\n - " + "\n - ".join(self.partyPlayer))
             else :
-                await reaction.message.channel.send("Vous n'etes pas le MDJ")
+                await reaction.message.channel.send(user.name + " vous n'etes pas le MDJ")
+    async def on_reaction_remove(self,reaction,user):
+        if (reaction.message.content == "Qui veut jouer ?" and reaction.emoji == "👍"):
+            self.partyPlayer.remove(user.name)
+        
+
 if __name__ == "__main__":
     DiscordBot().run(TOKEN) 
