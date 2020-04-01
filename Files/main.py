e63dd38a1b19a6985028dea8cdde85520d830616
sackboys 1 <florian.reimat@orange.fr>
2020-04-01 17:33:59 +0200

    1

--- Files/main.py
+++ Files/main.py
@@ -1,9 +1,12 @@
 import discord
 
-TOKEN = "Njk0ODg2NTE2MTE5NDM3NDEy.XoSgtw.LxN4m7fznpB8r8JNo_QO4SPs3yA"
+TOKEN = "Njk0ODg2NTE2MTE5NDM3NDEy.XoSpUA.7MEMBPkTwIWraaBLQLyUIKWpUnw"
 
 class DiscordBot(discord.Client) :
     def __init__(self):
+        self.partyIsLaunch = False
+        self.partyPlayer = []
+        self.owner = 0 
         super().__init__()
         
     async def on_ready(self):
@@ -15,17 +18,37 @@ class DiscordBot(discord.Client) :
             if (message.content == "Qui veut jouer ?"):
                 await message.add_reaction("👍")
                 await message.add_reaction("👎")
+                await message.add_reaction("▶️")
             return
         
-        if (message.content.startswith("lg!")):
-            await message.channel.send("Qui veut jouer ?")
+        if (message.content.startswith("lg!play")):
+            if (not(self.partyIsLaunch)):
+                self.partyIsLaunch = True
+                self.partyPlayer = []
+                self.owner = message.author
+                await message.channel.send("Qui veut jouer ?")
+            else :
+                await message.channel.send("Une partie est déja lancée")
+
+        if (message.content.startswith("lg!end")):
+            if (self.owner == message.author):
+                self.partyPlayer = []
+                self.partyIsLaunch = False
+                await message.channel.send("Fin de la partie")
+            else :
+                await message.channel.send("Vous n'etes pas le MDJ")
+
     async def on_reaction_add(self,reaction,user):
-        if (user.author == self.user)
+        if (user == self.user):
             return
-        print(user.name)
-        
-
-
-
+        if (reaction.message.content == "Qui veut jouer ?" and reaction.emoji == "👍"):
+            self.partyPlayer.append(user.name)
+        if (reaction.message.content == "Qui veut jouer ?" and reaction.emoji == "▶️"):
+            if (reaction.message.author == self.owner):
+                await reaction.message.channel.send("Liste des joueurs")
+                for i in self.partyPlayer:
+                    await reaction.message.channel.send("- " + i)
+            else :
+                await reaction.message.channel.send("Vous n'etes pas le MDJ")
 if __name__ == "__main__":
     DiscordBot().run(TOKEN) 
