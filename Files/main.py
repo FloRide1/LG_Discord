7d15331e13927885d00a4f61b8d934399791e853
sackboys 1 <florian.reimat@orange.fr>
2020-04-02 09:45:10 +0200

    0

--- Files/main.py
+++ Files/main.py
@@ -1,11 +1,11 @@
 import discord
 
-TOKEN = "Njk0ODg2NTE2MTE5NDM3NDEy.XoTruQ.kiuJVLkq2YlMN-mDzpYCd-Psxk4"
+TOKEN = "Njk0ODg2NTE2MTE5NDM3NDEy.XoWMiw.Jxl9y8B6WdQ3EHXfsryMrUWldhk"
 
 class DiscordBot(discord.Client) :
     def __init__(self):
         self.partyIsLaunch = False
-        self.partyPlayer = []
+        self.listPlayers = []
         self.owner = 0 
         self.channels = []
         super().__init__()
@@ -30,7 +30,7 @@ class DiscordBot(discord.Client) :
         if (message.content.startswith("lg!play")):
             if (not(self.partyIsLaunch)):
                 self.partyIsLaunch = True
-                self.partyPlayer = []
+                self.listPlayers = []
                 self.owner = message.author
                 await message.channel.send("Qui veut jouer ?")
             else :
@@ -38,7 +38,7 @@ class DiscordBot(discord.Client) :
 
         if (message.content.startswith("lg!end")):
             if (self.owner == message.author):
-                self.partyPlayer = []
+                self.listPlayers = []
                 self.partyIsLaunch = False
                 for i in self.channels:
                     await i.delete()
@@ -51,11 +51,11 @@ class DiscordBot(discord.Client) :
         if (user == self.user):
             return
         if (reaction.message.content == "Qui veut jouer ?" and reaction.emoji == "👍"):
-            self.partyPlayer.append(user)
+            self.listPlayers.append(user)
         if (reaction.message.content == "Qui veut jouer ?" and reaction.emoji == "▶️"):
             if (user == self.owner):
                 listP = "Liste des joueurs :"
-                for i in self.partyPlayer:
+                for i in self.listPlayers:
                     listP+= "\n - " + i.name 
                 await reaction.message.channel.send(listP)
                 
@@ -69,11 +69,29 @@ class DiscordBot(discord.Client) :
                 self.channels.append(chan)
                 chan = await self.guild.create_text_channel(name = "Village",category = category)
                 self.channels.append(chan)
+                game = Party(self.listPlayers,self.channels)
+                await game.on_configuration_update()
             else :
                 await reaction.message.channel.send(user.name + " vous n'etes pas le MDJ")
     async def on_reaction_remove(self,reaction,user):
         if (reaction.message.content == "Qui veut jouer ?" and reaction.emoji == "👍"):
-            self.partyPlayer.remove(user)
+            self.listPlayers.remove(user)
+
+
+
+class Party:
+    def __init__(self,listPlayers,channels):
+        self.listPlayers = listPlayers
+        self.channels = channels
+        self.lRoles = []
+
+    async def on_configuration_update(self):
+        strR = ("Roles : {0}/{1}").format(len(self.lRoles),len(self.listPlayers))
+        if (self.channels[1].last_message != None):
+            await self.channels[1].last_message.edit(content = strR)
+        else:
+            await self.channels[1].send(strR)
 
 if __name__ == "__main__":
     DiscordBot().run(TOKEN) 
+
